16fa9f3ac4a714e009a60263c239f33611b8535e
ItzDelay <05bryanchan05@gmail.com>
2026-08-05 15:00:37 +0700

    fix(api): remove default Content-Type header from axios instance to enable auto boundary generation

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -12,9 +12,6 @@ const isAbsoluteApiUrl = /^https?:\/\//i.test(API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 api.interceptors.request.use((config) => {
